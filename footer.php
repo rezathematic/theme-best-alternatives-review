e1565d60a5a699f506f036ec4be6a4f57a2e297f
Reza <r.bahar91@gmail.com>
2021-09-18 18:48:22 -0700

    Update: added notes

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<!-- Put this in the footer: Best Alternative Reviews is a participant in the Amazon Services LLC Associates Program, an affiliate advertising program designed to provide a means for sites to earn advertising fees by advertising and linking to Amazon.com. -->
 <?php wp_footer(); ?>
 </body>
 
